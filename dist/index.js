96edd5232b809c37b0005bf508cb43cd87c970b7
Dreace233 <Dreace@Foxmail.com>
2019-04-12 22:54:03 +0800

    Version 1.9.9
     1. 新功能：全校无课教室查询
     2. 注：可在 更多->自习室 使用查询功能

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,6 @@ import $wuxCountUp from './countup/index'
  */
 const getCtx = (selector, ctx = getCurrentPages()[getCurrentPages().length - 1]) => {
     const componentCtx = ctx.selectComponent(selector)
-
     if (!componentCtx) {
         throw new Error('无法找到对应的组件，请按文档说明使用组件')
     }
